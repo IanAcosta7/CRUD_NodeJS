ff457f6fd916124b59ace7e6792adf680a57ab68
IanAcosta7 <ianacosta700@gmail.com>
2019-03-28 23:22:37 -0300

    Enviroment Variable for credentials

--- server/config/config.js
+++ server/config/config.js
@@ -7,6 +7,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // Database
 let DB_URL;
 
-process.env.NODE_ENV === 'dev' ?  DB_URL = 'mongodb://localhost:27017/CRUD' : DB_URL = 'mongodb+srv://Ian:TZMYcrN4uroG8BNV@cluster0-nsp2p.mongodb.net/CRUD';
+process.env.NODE_ENV === 'dev' ?  DB_URL = 'mongodb://localhost:27017/CRUD' : DB_URL = process.env.MONGO_URI;
 
 process.env.DB_URL = DB_URL;
